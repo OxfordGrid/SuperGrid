ee5a12d41198d043e9aed494d406fff7c6ad5a9e
8390733 <8390733@W7D5244>
2016-04-12 11:52:25 +0100

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
+
+namespace OwinSelfhostSample
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string baseAddress = "http://localhost:9000/";
+            Console.WriteLine($"Student is running at {baseAddress}");
+            
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
